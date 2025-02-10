5b5b8aac3a7d127c3f80f947210eff6329494d37
Sameer Singh <sameersingh2799@gmail.com>
2025-02-10 20:10:42 +0530

    Some error corrected!

--- pr/config.py
+++ pr/config.py
@@ -20,11 +20,11 @@ class DevelopementConfig(Config):
 class ProdConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
-    SECRET_KEY = 'c09612a7ea2d5260785c12780ec2ea222d8eaa05550b028041c8ea357026875e'  # secrets.token_hex()
-    SECURITY_PASSWORD_SALT = 'c09612a7ea2d5260785c12780ec2ea222d8eaa05550b028041c8ea357026875e'  # secrets.SystemRandom().getrandbits(128)
+    SECRET_KEY = 'key'  # secrets.token_hex()
+    SECURITY_PASSWORD_SALT = 'key'  # secrets.SystemRandom().getrandbits(128)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False
-    SECURITY_TOKEN_AUTHENTICATION_HEADER = 'c09612a7ea2d5260785c12780ec2ea222d8eaa05550b028041c8ea357026875e'
+    SECURITY_TOKEN_AUTHENTICATION_HEADER = 'key'
     CACHE_REDIS_HOST = "localhost"
     CACHE_REDIS_PORT = 6379
     CACHE_REDIS_DB = 3
